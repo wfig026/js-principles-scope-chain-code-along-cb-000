c670eae0f4733ead17f1752ff70f09acabd1a6dd
Will Figueiredo <will@wfigueiredo.com>
2019-03-31 03:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
 
-// Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'
+/* Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'
+   Also, poor us, declaring a global variable from inside a function is one of those things we would never want to do normally, 
+   but is good for us to explore right now. */
 function setBestCustomer() {
   bestCustomer = 'not bob';
+}
+
+// See the consequences of declaring a variable in global scope, by writing a new function called overWriteBestCustomer that changes that bestCustomer variable
+function overWriteBestCustomer() {
+  bestCustomer = 'will'
 }
