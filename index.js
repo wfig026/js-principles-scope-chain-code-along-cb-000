88d942123dd6a3f10e224e75317ce16131e37be0
Will Figueiredo <will@wfigueiredo.com>
2019-03-31 03:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 
 // Declare a variable in global scope called customerName using the var keyword.
 var customerName = 'bob';
+
+// Write a function that accesses that global customerName variable, and uppercases it.
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
